e7d6eb3b246e2cb2824dceca621935f7b8b4fbab
Yuichi Murata <mrk21info+github@gmail.com>
2018-05-26 17:24:24 +0900

    js_vexflow: Hello World

---
+++ js_vexflow/README.md
@@ -0,0 +1,14 @@
+# js verxflow
+
+![screenshot.png](./screenshot.png)
+
+## Setup
+```bash
+$ nodenv install
+$ npm install
+$ npm start
+$ open http://localhost:8080
+```
+## Refer to
+
+* [0xfe/vexflow: A JavaScript library for rendering music notation and guitar tablature.](https://github.com/0xfe/vexflow)
---
+++ js_vexflow/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'dist/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'],
+        },
+      }
+    ],
+  },
+};
