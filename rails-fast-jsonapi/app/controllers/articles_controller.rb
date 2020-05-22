d50ab56b5ad2ca53003d68007d4f19028aa786a5
Yuichi Murata <mrk21info+github@gmail.com>
2020-05-22 14:01:32 +0900

    rails-fast-jsonapi: Add kaminari

--- rails-fast-jsonapi/app/controllers/articles_controller.rb
+++ rails-fast-jsonapi/app/controllers/articles_controller.rb
@@ -1,10 +1,26 @@
 class ArticlesController < ApplicationController
   def index
-    articles = Article.all
+    articles = Article.page(params[:page]).per(params[:per])
     options = {
+      meta: {
+        pagination: make_pagination_info(articles)
+      },
       include: [:author]
     }
     json_string = ArticleSerializer.new(articles, options).serialized_json
     render json: json_string
+  end
+
+  private
+
+  def make_pagination_info(records)
+    {
+      total_count: records.total_count,
+      per_page: records.limit_value,
+      current_page: records.current_page,
+      current_page: 1,
+      next_page: records.next_page,
+      prev_page: records.prev_page,
+    }
   end
 end
