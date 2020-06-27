100960f07012932be141d5ad578ccfc001f7c585
Yuichi Murata <mrk21info+github@gmail.com>
2020-06-28 00:25:58 +0900

    unity-syphon-v2019: Add

---
+++ unity-syphon-v2019/Assets/UnitySyphon/Script/WebCameraBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace UnitySyphon
+{
+    public class WebCameraBehaviour : MonoBehaviour
+    {
+        IEnumerator Start()
+        {
+            yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+            if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+            {
+                Debug.Log("webcam found");
+                StartWebcam();
+            }
+            else
+            {
+                Debug.Log("webcam not found");
+            }
+        }
+
+        void StartWebcam()
+        {
+            var userCameraDevices = WebCamTexture.devices.ToList();
+            userCameraDevices.ForEach((v) => { Debug.Log(v.name); });
+            var camera = userCameraDevices.Where((v) => v.name == "USB_Camera").First();
+            var webCamTexture = new WebCamTexture(camera.name, 1920 / 3, 1080 / 3, 30);
+            GetComponent<Renderer>().material.mainTexture = webCamTexture;
+            webCamTexture.Play();
+        }
+    }
+}
---
+++ unity-syphon-v2019/README.md
@@ -0,0 +1,25 @@
+# Unity Syphon for Unity 2019.x
+
+## Dependencies
+
+- Unity 2019.4.1f1
+- [CamTwist](http://camtwiststudio.com/)
+  - Virtual camera devise
+  - Syphon client
+
+### Assets
+
+- [KlakSyphon](https://github.com/keijiro/KlakSyphon)
+  - [0.0.3](https://github.com/keijiro/KlakSyphon/releases/tag/v0.0.3)
+
+## Setup
+
+1. Import assets
+
+## Refer to
+
+- [【悲報】MacのZoomを最新版にアップデートするとSnap Cameraが使えない状態になります泣→ 解決方法見つかりました（5月9日更新）｜宇樹義子（そらき・よしこ）｜note](https://note.com/decinormal/n/n5d551b907620)
+- [【Unity】WebCamTextureを使ってカメラが映している画像情報を取得するよ - うら干物書き](https://www.urablog.xyz/entry/2017/07/06/225444)
+- [Syphon: Syphon Framework](http://syphon.v002.info/FrameworkDocumentation/)
+- [Syphon/Simple: Simple Client and Simple Server demo and testbed applications.](https://github.com/Syphon/Simple)
+- [UnityでのWebカメラの使い方｜npaka｜note](https://note.com/npaka/n/nbaa0e466b0de)
