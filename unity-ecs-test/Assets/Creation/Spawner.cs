8c809d6a8053d39aff3d9be8c6c8ac05f6f64311
Yuichi Murata <mrk21info+github@gmail.com>
2021-02-28 00:23:54 +0900

    unity-ecs-test: Modify

--- unity-ecs-test/Assets/Creation/Spawner.cs
+++ unity-ecs-test/Assets/Creation/Spawner.cs
@@ -21,25 +21,39 @@ namespace Creator
                 typeof(RenderBounds),
                 typeof(LocalToWorld),
                 typeof(Scaler)
-                );
-            Entity entity = manager.CreateEntity(archetype);
+            );
 
-            manager.AddComponentData(entity, new Translation
-            {
-                Value = new Unity.Mathematics.float3(2f, 0f, 4f)
-            });
-
-            manager.AddComponentData(entity, new Scaler
-            {
-                Value = 2.0f,
-                T = 90
-            });
-
-            manager.AddSharedComponentData(entity, new RenderMesh
+            var renderMesh = new RenderMesh
             {
                 mesh = mesh,
                 material = material,
-            });
+            };
+
+            var rand = new System.Random();
+
+            for (int x = -5; x <= 5; x++)
+            {
+                for (int y = -5; y <= 5; y++)
+                {
+                    for (int z = -5; z <= 5; z++)
+                    {
+                        Entity entity = manager.CreateEntity(archetype);
+
+                        manager.AddComponentData(entity, new Translation
+                        {
+                            Value = new Unity.Mathematics.float3(x, y, z)
+                        });
+
+                        manager.AddComponentData(entity, new Scaler
+                        {
+                            Value = (float)rand.NextDouble() + 1.0f,
+                            T = rand.Next(0, 360)
+                        });
+
+                        manager.AddSharedComponentData(entity, renderMesh);
+                    }
+                }
+            }
         }
     }
 }
