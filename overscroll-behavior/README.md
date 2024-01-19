0715f2873a7f57a633019ade3bc8b7d9fa831558
Yuichi Murata <mrk21info+github@gmail.com>
2024-01-19 19:21:27 +0900

    overscroll-vehavior: Add

---
+++ overscroll-behavior/README.md
@@ -0,0 +1,22 @@
+# overscroll behavior
+
+## Dependencies
+
+- Node.js: 20.x
+- Next.js: 14.x
+- asdf
+  - Multiple Runtime Version Manager
+
+## Getting Started
+
+```bash
+asdf install
+npm install
+npm run dev
+open http://localhost:3000
+```
+
+## References
+
+- [Next.js](https://nextjs.org/)
+- [overscroll-behaviorがお手軽！ モーダルUI等のスクロール連鎖を防ぐ待望のCSS - ICS MEDIA](https://ics.media/entry/221024/)
---
+++ overscroll-behavior/src/app/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+import { useCallback, useState } from "react";
+
+function *times(n: number) {
+  for (let i = 0; i < n; i++) {
+    yield i;
+  }
+}
+
+export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const onClickOpenButton = useCallback(() => setIsOpen(true), [setIsOpen]);
+  const onClickCloseButton = useCallback(() => setIsOpen(false), [setIsOpen]);
+
+  return (
+    <div className="content">
+      <button className="open-button" type="button" onClick={onClickOpenButton}>open modal</button>
+      { [...times(1000)].map((m,i) => 
+        <p key={i}>Message {m}.</p>
+      ) }
+
+      <div className={`modal ${isOpen ? 'open' : ''}`}>
+        <div className="modal-content">
+          <div className="modal-header">
+            <h2>Modal Header</h2>
+          </div>
+          <div className="modal-body">
+            { [...times(10)].map((m,i) => 
+              <p key={i}>Message {m}.</p>
+            ) }
+          </div>
+          <div className="modal-footer">
+            <button onClick={onClickCloseButton}>OK</button>
+            <button onClick={onClickCloseButton}>Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
